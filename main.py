279ecb8db79122e4116defc4824a0203523fab40
ayoub <ayoubhaddou1@gmail.com>
2022-05-12 16:26:16 +0200

    api first commit

---
+++ main.py
@@ -0,0 +1,74 @@
+
+from fastapi import FastAPI 
+import uvicorn 
+import pickle 
+import numpy as np 
+import json 
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import texthero as hero 
+from texthero import stopwords
+
+
+# # Save le model : 
+# model = ...  # Get model (Sequential, Functional Model, or Model subclass)
+# model.save('path/to/location')
+
+# # Load model 
+# model = keras.models.load_model('model89')
+
+app = FastAPI()
+
+def preprocessing_pred(sentence):
+    texte = np.array([sentence])
+    df_pred = pd.DataFrame(texte)
+    df_pred.columns = ['Texte']
+
+    default_stopwords = stopwords.DEFAULT
+    custom_stopwords = default_stopwords.union(set(["feel",'aa','ab','http','anything','less','ever',"im",'href','actualy','enough','actually','look','come','sure','many','I','got','able','year','help','take','feels','felt','like','lot','go','around','makes','back','work','need','pretty','way','though','see','ive','good','also','right','dont','cant','say','didnt','could','even','day','every','make','made','much','going','one',"feeling",'things','something',"feelings",'always','never','today','days','life',"like","really",'know','time','get','little','bit','would','want','think','people','still']))
+    texte = hero.remove_urls(df_pred.Texte)
+    texte = hero.remove_stopwords(texte, custom_stopwords)
+    texte = hero.remove_punctuation(texte)
+    texte = hero.remove_digits(texte)
+    texte = hero.remove_urls(texte)
+
+    # loading tokenizer
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # Text to sequences 
+    df_pred['processed'] = tokenizer.texts_to_sequences(df_pred.Texte)
+
+    max_length = 30
+    pad_sentences = pad_sequences(df_pred.processed, maxlen=max_length,padding='post')
+
+    model = keras.models.load_model('model89')
+    result = np.argmax(model.predict(np.array(pad_sentences)))
+
+    if result == 0 :
+        return 'sadness'
+    elif result == 1 :
+        return 'anger'
+    elif result == 2 :
+        return 'love'
+    elif result == 3 :
+        return 'surprise' 
+    elif result == 4 :
+        return 'fear'
+    elif result == 5 :
+        return 'happy'
+    else:
+        return 'error'
+
+@app.post("/predict/")
+def prediction_lgbm(Data):
+    return preprocessing_pred(Data)
+
+@app.get("/")
+def pred():
+    return {'Bonjour' : "Bonjour"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+
---
+++ model89/keras_metadata.pb
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:68e927b38b63426b304bb5babcee2345b64da8f885dd77281488fc3753f003a3
+size 182184349
